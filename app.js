a38d4e1459430db51a4b46310572dd114ef3c234
Juan Bautista Fabrizi <67335783+JuanBautistaFabrizi@users.noreply.github.com>
2021-09-24 11:05:29 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+function store(){
+
+    var name = document.getElementById('name');
+    var pw = document.getElementById('pw');
+    var lowerCaseLetters = /[a-z]/g;
+    var upperCaseLetters = /[A-Z]/g;
+    var numbers = /[0-9]/g;
+
+    if(name.value.length == 0){
+        alert('Ponga su emails');
+
+    }else if(pw.value.length == 0){
+        alert('Por favor pongas su paswords');
+
+    }else if(name.value.length == 0 && pw.value.length == 0){
+        alert('Por favor ponga sus emails y su paswords');
+
+    }else if(pw.value.length > 8){
+        alert('No puede tener más de 8 caracteres');
+
+    }else if(!pw.value.match(numbers)){
+        alert('Agregue un numeros');
+
+    }else if(!pw.value.match(upperCaseLetters)){
+        alert('Agregue una mayúsculas');
+
+    }else if(!pw.value.match(lowerCaseLetters)){
+        alert('Agregue una minúsculas');
+
+    }else{
+        localStorage.setItem('name', name.value);
+        localStorage.setItem('pw', pw.value);
+        alert('Su cuentas ha sido creadas');
+        window.location.href ="http://127.0.0.1:5500/login.html";
+    }
+}
+
+
+function check(){
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('pw');
+
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
+    var userRemember = document.getElementById("rememberMe");
+
+    if(userName.value == storedName && userPw.value == storedPw){
+        alert('Estás logueados');
+        window.location.replace("./menus.html");
+        
+    }else{
+        alert('Error logueandotes');
+    }
+}
+        
+function bienvenido() {
+    window.location.replace("./login.html");
+}
+        
+function bebidasAlcoholicas() {
+    let edad = prompt("Decinos tu edad");
+    edad = parent(edad);
+    if (edad >= 18) {
+        window.location.replace("./bebidasalcoholicas.html");
+    }else{
+
+    }
+}
+        
+
+
